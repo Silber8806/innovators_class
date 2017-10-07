34f70d8c12da7256826c56edb307d5fbefdc18a3
Chris Kottmyer <ckottmyer@ckottmyer-ltm.internal.salesforce.com>
2017-10-06 23:58:39 -0400

    updating

--- lesson4.py
+++ lesson4.py
@@ -1,31 +1,24 @@
-# laright, data types
-print("to specifiy data types...")
-print("you need to use type(<data type) function")
+# A string is any set of keystrokes surrounded by single,
+# double or triple quotes.  eg:
+# single quote: '
+# double quote: "
+# triple quote: """
+# Why the difference?
 
-print("this is a number...")
-print(9)
-
-print("this is it's data type...")
-print(type(9))
-
-print("double quoted sentence is a string")
-print(type("this is a string"))
-
-print("What happens, when we divide an integer / integer")
-print(3/2)
-
-print("we get an integer...")
-print("what about a integer divided by float?")
-print(3/2.0)
-
-print("if we want to make it integer division we use //")
-print(3//2.0)
-
-print("what about a integer / string")
-print(3 / "string")
-
-# this is our first error/bug/execption.  A string can't be divided
-# added to an integer.
-
-print("nothing happens after this point!")
-print(1 + 1)
+print('this is a single quoted string')
+print('single quotes let me use "double" quotes!')
+print('if I used double quotes inside double quoted strings')
+print('the computer would not know where the string starts...')
+print
+print("alternatively double quotes let me use single quotes")
+print("I wouldn't lie to you about that single quote?")
+print
+print("This is all useful, but sometimes I want to use both")
+print("double and single quotes or maybe even write a whole")
+print("paragraph without having to write each line...")
+print("""
+This is where the triple quotes come in!
+I can write as many lines as I want.
+    I can add spaces...
+I can even add single ' and double quotes "
+""")
