ab988310d68ea24efababfccd074bf692b3d024f
Chris Kottmyer <ckottmyer@ckottmyer-ltm.internal.salesforce.com>
2017-10-21 21:20:37 -0400

    updating

---
+++ Class3/lesson4.py
@@ -0,0 +1,19 @@
+# this is an if construct
+
+ILoveCats = 1
+
+if (ILoveCats == 1):
+	print("I Love my Cat!")
+	print("My Cat is the Best!")
+	print("My Cat's name is Whiskers!")
+
+# test where code is not pre
+
+ILoveCats = 0
+
+if (ILoveCats == 1):
+	print("I Love my Cat!")
+	print("My Cat is the Best!")
+	print("My Cat's name is Whiskers!")
+
+	
---
+++ Class3/lesson5.py
@@ -0,0 +1,19 @@
+ILoveCats = "<?>"
+
+if (ILoveCats == 1):
+	print("I Love my Cat!")
+	print("My Cat is the Best!")
+	print("My Cat's name is Whiskers!")
+
+print("Do you think I love cats?")
+
+# if ILoveCats = 0
+ILoveCats = 0
+print("Do you think I love cats?")
+
+# if ILoveCats = 1
+ILoveCats = 1
+print("I Love my Cat!")
+print("My Cat is the Best!")
+print("My Cat's name is Whiskers!")
+print("Do you think I love cats?")
