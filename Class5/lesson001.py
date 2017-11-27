622a2e1bb46a9ca3fce1a994fc8ee5e9b3521fe8
Chris Kottmyer <ckottmyer@ckottmyer-ltm.internal.salesforce.com>
2017-11-26 20:34:47 -0500

    class_five

---
+++ Class5/lesson001.py
@@ -0,0 +1,9 @@
+alphabet='abcdefghijklmnopqrstuvwxyz'
+
+print(alphabet[0])
+print(alphabet[1])
+print(alphabet[25])
+print(alphabet[-1])
+print(alphabet[-25])
+print(alphabet[-26])
+print(alphabet[30])
---
+++ Class5/lesson003.py
@@ -0,0 +1,15 @@
+alphabet='abcdefghijklmnopqrstuvwxyz'
+
+print(alphabet[0:1])
+print(alphabet[1:2])
+print(alphabet[0:2])
+print(alphabet[0:13])
+print(alphabet[:13])
+print(alphabet[13:])
+print(alphabet[0:-1])
+print(alphabet[-10:-1])
+print(alphabet[:-1])
+
+print("This prints nothing:")
+print(alphabet[10:1])
+print(alphabet[-1:-10])
